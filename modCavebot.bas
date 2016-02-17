blackdtools/Blackd-Proxy-CLASSIC

commit 43f9af554f9fa9b29de2bb5b0f21f471120ce688

fix cavebot spam attack packet bug (again)
currently, the cavebot will spam the same attack packet on the same creature several times while fighting... fix that, so we only send the attack packet if the red square is not around that creature. see https://github.com/blackdtools/Blackd-Proxy-CLASSIC/issues/57  for more info

this bug is probably detectable by cipsoft.

modCavebot.bas
@@ -2432,10 +2432,11 @@ Public Function MeleeAttack(idConnection As Integer, targetID As Double, Optiona
                         sCheat = "05 00 A1 " & SpaceID(targetID)
                     End If
                 
-                
-                    WriteRedSquare idConnection, currTargetID(idConnection) ' new since in tibia 8.62
-                    inRes = GetCheatPacket(cPacket, sCheat)
-                    frmMain.UnifiedSendToServerGame idConnection, cPacket, True
+                    If (currTargetID(idConnection) <> currentRedSquare) Then 'don't resend attack packet if we are already attacking this creature (the client will always notice if attack has been canceled, so currentRedSquare should always be up to date, i believe.. )
+                       WriteRedSquare idConnection, currTargetID(idConnection) ' new since in tibia 8.62
+                       inRes = GetCheatPacket(cPacket, sCheat)
+                       frmMain.UnifiedSendToServerGame idConnection, cPacket, True
+                    End If
                     DoEvents
                 End If
             End If

commit 0e2477139f503affec08f6257f3e30c4a3e974ba

fix RightNumberOfClicks counter
broke it in https://github.com/blackdtools/Blackd-Proxy-CLASSIC/commit/43f9af554f9fa9b29de2bb5b0f21f471120ce688

modCavebot.bas
@@ -2419,6 +2419,7 @@ Public Function MeleeAttack(idConnection As Integer, targetID As Double, Optiona
                             End If
                         End If
                     End If
+                    If (currTargetID(idConnection) <> currentRedSquare) Then 'don't resend attack packet if we are already attacking this creature (the client will always notice if attack has been canceled, so currentRedSquare should always be up to date, i believe.. )
                     If TibiaVersionLong >= 860 Then
                         safeRes = RightNumberOfClicks(idConnection)
                         If safeRes = "" Then
@@ -2432,7 +2433,6 @@ Public Function MeleeAttack(idConnection As Integer, targetID As Double, Optiona
                         sCheat = "05 00 A1 " & SpaceID(targetID)
                     End If
                 
-                    If (currTargetID(idConnection) <> currentRedSquare) Then 'don't resend attack packet if we are already attacking this creature (the client will always notice if attack has been canceled, so currentRedSquare should always be up to date, i believe.. )
                        WriteRedSquare idConnection, currTargetID(idConnection) ' new since in tibia 8.62
                        inRes = GetCheatPacket(cPacket, sCheat)
                        frmMain.UnifiedSendToServerGame idConnection, cPacket, True